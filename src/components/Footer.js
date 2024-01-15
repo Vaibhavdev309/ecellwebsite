566401ca9e5f0e6a301d02ab0d70d294f5b024c1
Raman01010010 <rmnprjrrr@gmail.com>
2024-01-16 00:32:50 +0530

    added hiver effects

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,32 +8,30 @@ export const Footer = () => {
     <div className="container px-5 py-24 mx-auto flex  lg:items-start md:flex-row justify-center  flex-wrap flex-col">
       <div className=" flex-shrink-0  text-center ">
       <div className='flex justify-center'>
-          <img  className="h-60 text-white  " src='https://e-cellmnnit.netlify.app/static/media/logo1.aa59d10dce000c1b6a45.png'/>
+          <img  className="h-60 text-white  hover:scale-125 transition-transform transform " src='https://e-cellmnnit.netlify.app/static/media/logo1.aa59d10dce000c1b6a45.png'/>
    
           </div>
     
-        <p className=" flex justify-center text-2xl text-gray-100">
+        <p className=" flex justify-center text-2xl text-gray-100 hover:scale-125 transition-transform transform">
           E-Cell MNNIT
         </p>
       </div>
       <div className="flex-grow flex flex-wrap -mb-10  mt-10  text-center">
         <div className="lg:w-1/2 md:w-1/2  w-full px-4">
-          <h2 className="title-font text-center  text-black tracking-widest text-2xl lg:text-4xl rounded-md font-semibold bg-gradient-to-r from-green-600 to-[#92f242] p-6 mb-3">
+          <h2 className="title-font text-center hover:scale-110 transition-transform transform  text-black tracking-widest text-2xl lg:text-4xl rounded-md font-semibold bg-gradient-to-r from-green-600 to-[#92f242] p-6 mb-3">
           GET IN TOUCH
           </h2>
           <nav className="list-none mb-10">
             <li>
-              <a className="flex justify-between mb-2  text-gray-400 text-2xl hover:text-white"> <img className='h-[8vh] bg-gray-300 rounded-xl p-1' src={location}></img>E-Cell, MNNIT Allahabad, India</a>
+              <a className="flex justify-between mb-2  text-gray-400 text-2xl hover:text-white"> <img className=' hover:scale-125 transition-transform transform h-[8vh] bg-gray-300 rounded-xl p-1' src={location}></img>E-Cell, MNNIT Allahabad, India</a>
             </li>
             <li>
-              <a className="flex justify-between mb-2 text-2xl text-gray-400 hover:text-white">   <img className='h-[8vh] bg-gray-300 rounded-xl p-1' src={contact}></img> +91 7726924556</a>
+              <a className="flex justify-between mb-2 text-2xl text-gray-400 hover:text-white">   <img className='hover:scale-125 transition-transform transform h-[8vh] bg-gray-300 rounded-xl p-1' src={contact}></img> +91 7726924556</a>
             </li>
             <li>
-              <a className="flex justify-between break-all text-2xl text-gray-400 hover:text-white"><img className='h-[8vh] bg-gray-300 rounded-xl p-1' src={email}></img> support@ecellmnnit.com</a>
+              <a className="flex justify-between break-all text-2xl text-gray-400 hover:text-white"><img className='hover:scale-125 transition-transform transform h-[8vh] bg-gray-300 rounded-xl p-1' src={email}></img> support@ecellmnnit.com</a>
             </li>
-            <li>
-              <a className="text-gray-400 hover:text-white">Fourth Link</a>
-            </li>
+           
           </nav>
         </div>
        
@@ -44,6 +42,7 @@ export const Footer = () => {
   title="map"
   width="100%"
   height="100%"
+  className='rounded-xl '
   frameBorder={0}
   marginHeight={0}
   marginWidth={0}
@@ -55,7 +54,7 @@ export const Footer = () => {
     </div>
     <div className="bg-gray-800 bg-opacity-75">
       <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-        <p className="text-gray-400 hover:text-gray-100 text-lg text-center sm:text-left">
+        <p className="hover:scale-110 transition-transform transform text-gray-400 hover:text-gray-100 text-lg text-center sm:text-left">
           © 2024 E-Cell MNNIT 
         
         </p>
@@ -66,7 +65,7 @@ export const Footer = () => {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth={2}
-              className="w-10 h-10"
+              className="w-10 h-10 hover:scale-125 transition-transform transform"
               viewBox="0 0 24 24"
             >
               <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
@@ -78,7 +77,7 @@ export const Footer = () => {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth={2}
-              className="w-10 h-10"
+              className="w-10 h-10 hover:scale-125 transition-transform transform"
               viewBox="0 0 24 24"
             >
               <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
@@ -91,7 +90,7 @@ export const Footer = () => {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth={2}
-              className="w-10 h-10"
+              className="w-10 h-10 hover:scale-125 transition-transform transform"
               viewBox="0 0 24 24"
             >
               <rect width={20} height={20} x={2} y={2} rx={5} ry={5} />
@@ -105,7 +104,7 @@ export const Footer = () => {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth={0}
-              className="w-10 h-10"
+              className="w-10 h-10 hover:scale-125 transition-transform transform"
               viewBox="0 0 24 24"
             >
               <path
