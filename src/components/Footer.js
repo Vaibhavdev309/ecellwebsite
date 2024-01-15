0bcdd89aada7ca7ab871115475e7313d5e076ba2
Raman01010010 <rmnprjrrr@gmail.com>
2024-01-15 23:21:50 +0530

    added map preview

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -53,44 +53,20 @@ export const Footer = () => {
             </li>
           </nav>
         </div>
-        <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-          <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">
-            CATEGORIES
-          </h2>
-          <nav className="list-none mb-10">
-            <li>
-              <a className="text-gray-400 hover:text-white">First Link</a>
-            </li>
-            <li>
-              <a className="text-gray-400 hover:text-white">Second Link</a>
-            </li>
-            <li>
-              <a className="text-gray-400 hover:text-white">Third Link</a>
-            </li>
-            <li>
-              <a className="text-gray-400 hover:text-white">Fourth Link</a>
-            </li>
-          </nav>
+        <div className="lg:w-1/2 md:w-1/2 w-full px-4">
+          
+          <iframe
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7202.697571927987!2d81.85372687770996!3d25.493411800000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399aca78818ddc51%3A0x6690dd2de3a1415b!2sMotilal%20Nehru%20National%20Institute%20of%20Technology%2C%20Allahabad!5e0!3m2!1sen!2sin!4v1689057849063!5m2!1sen!2sin"
+  title="map"
+  width="100%"
+  height="100%"
+  frameBorder={0}
+  marginHeight={0}
+  marginWidth={0}
+  scrolling="no"
+/>
         </div>
-        <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-          <h2 className="title-font font-medium text-white tracking-widest text-sm mb-3">
-            CATEGORIES
-          </h2>
-          <nav className="list-none mb-10">
-            <li>
-              <a className="text-gray-400 hover:text-white">First Link</a>
-            </li>
-            <li>
-              <a className="text-gray-400 hover:text-white">Second Link</a>
-            </li>
-            <li>
-              <a className="text-gray-400 hover:text-white">Third Link</a>
-            </li>
-            <li>
-              <a className="text-gray-400 hover:text-white">Fourth Link</a>
-            </li>
-          </nav>
-        </div>
+        
       </div>
     </div>
     <div className="bg-gray-800 bg-opacity-75">
